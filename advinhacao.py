3e3ef57977e9adaaa128b1b7e085b5e6087fe7d0
henrique-a-f <henrique-a-f@live.com>
2023-01-31 15:15:29 -0300

    Implementado for no laço de repetição.

--- advinhacao.py
+++ advinhacao.py
@@ -6,9 +6,8 @@ print("*"*32)
 
 numero_secreto = randint(0, 100)
 total_de_tentativas = 3
-rodada = 1
 
-while (rodada <= total_de_tentativas):
+for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
     chute = int(input("Digite o seu número: "))
     print("Você digitou: ", chute)
@@ -24,8 +23,6 @@ while (rodada <= total_de_tentativas):
             print("Você errou! O seu chute foi maior que o número secreto.")
         elif (menor):
             print("Você errou! o seu número foi menor que o número secreto.")
-
-    rodada = rodada + 1
 
 print("O número secreto era: ", numero_secreto)
 print("Fim do jogo")
