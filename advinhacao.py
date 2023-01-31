131578ed3643b5995408d2f46a8f757722412120
henrique-a-f <henrique-a-f@live.com>
2023-01-31 16:35:53 -0300

    adicionado níveis de dificuldade

--- advinhacao.py
+++ advinhacao.py
@@ -5,7 +5,21 @@ print("Bem Vindo ao jogo de Advinhação!")
 print("*"*32)
 
 numero_secreto = randint(1, 100)
-total_de_tentativas = 3
+total_de_tentativas = 0
+
+print("Qual nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defina um nível: "))
+
+if (nivel == 1):
+    total_de_tentativas = 20
+elif (nivel == 2):
+    total_de_tentativas = 10
+elif (nivel == 3):
+    total_de_tentativas = 5
+else:
+    print("Valor de dificuldade incorreto, escolhe entre 1 à 3")
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
