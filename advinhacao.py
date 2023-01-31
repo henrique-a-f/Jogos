bd344763989dea8e4b379ff2a7f330e984dfa1ad
henrique-a-f <henrique-a-f@live.com>
2023-01-31 15:51:34 -0300

    Corrigido formatações de saída e range de números randomicos.

--- advinhacao.py
+++ advinhacao.py
@@ -4,13 +4,13 @@ print("*"*32)
 print("Bem Vindo ao jogo de Advinhação!")
 print("*"*32)
 
-numero_secreto = 42  # randint(0, 100)
+numero_secreto = randint(1, 100)
 total_de_tentativas = 3
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
     chute = int(input("Digite um número entre 1 e 100: "))
-    print("Você digitou: ", chute)
+    print("Você digitou: {:3d}".format(chute))
 
     if (chute < 1 or chute > 100):
         print("Você deve digitar um número entre 1 e 100!")
@@ -29,5 +29,5 @@ for rodada in range(1, total_de_tentativas + 1):
         elif (menor):
             print("Você errou! o seu número foi menor que o número secreto.")
 
-print("O número secreto era: ", numero_secreto)
+print("O número secreto era: {:3d}".format(numero_secreto))
 print("Fim do jogo")
