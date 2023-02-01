32081fcb13ecd26decf0d3ef261694725ec3cbbc
henrique-a-f <henrique-a-f@live.com>
2023-02-01 16:16:03 -0300

    Colocando algumas lógicas dentro de funções, para melhorar a visualização e entendimento do código.

--- forca.py
+++ forca.py
@@ -2,22 +2,10 @@ import random
 
 
 def jogar():
-    print("*"*34)
-    print("*"*3, "Bem Vindo ao jogo da Forca!", "*"*3)
-    print("*"*34)
 
-    arquivo = open("palavras.txt", "r")
-    palavras = []
-    for linha in arquivo:
-        linha = linha.strip()
-        palavras.append(linha)
-
-    arquivo.close()
-
-    numero = random.randrange(0, len(palavras))
-
-    palavra_secreta = palavras[numero].upper()
-    letras_acertadas = ["_" for letra in palavra_secreta]
+    imprime_mensagem_abertura()
+    palavra_secreta = carrega_palavra_secreta()
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
 
     enforcou = False
     acertou = False
@@ -48,6 +36,29 @@ def jogar():
     else:
         print("Não foi dessa vez. Você perdeu!")
     print("Fim do jogo")
+
+
+def imprime_mensagem_abertura():
+    print("*"*34)
+    print("*"*3, "Bem Vindo ao jogo da Forca!", "*"*3)
+    print("*"*34)
+
+
+def carrega_palavra_secreta():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    return palavra_secreta
+
+
+def inicializa_letras_acertadas(palavra):
+    return ["_" for letra in palavra]
 
 
 if (__name__ == "__main__"):
