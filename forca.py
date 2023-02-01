2b541b44ce209fa7c1ac6e2fd934b33bf756c010
henrique-a-f <henrique-a-f@live.com>
2023-02-01 12:06:45 -0300

    Criando variaveis do jogo forca e dando inicio ao laço de repetição.

--- forca.py
+++ forca.py
@@ -1,9 +1,15 @@
-
-
 def jogar():
-    print("*"*33)
-    print("*"*2, "Bem Vindo ao jogo de Forca!", "*"*2)
-    print("*"*33)
+    print("*"*34)
+    print("*"*3, "Bem Vindo ao jogo da Forca!", "*"*3)
+    print("*"*34)
+
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+        print("jogando ...")
 
     print("Fim do jogo")
 
